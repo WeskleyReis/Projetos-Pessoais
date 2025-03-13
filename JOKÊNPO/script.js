5b1fbb5456ceb0e621b14578e1d8e2d941350a8c
WeskleyReis <weskleyff88004216@gmail.com>
2025-03-13 17:28:43 -0300

    Update Jokênpo-game

--- JOKÊNPO/script.js
+++ JOKÊNPO/script.js
@@ -2,6 +2,10 @@ document.getElementById('pedra').addEventListener('click', () => escolha('pedra'
 document.getElementById('papel').addEventListener('click', () => escolha('papel'))
 document.getElementById('tesoura').addEventListener('click', () => escolha('tesoura'))
 
+const result = document.querySelector('.result')
+const score = document.querySelector('.score')
+const choices = document.querySelector('.choices')
+
 let vitorias = 0
 let derrotas = 0
 let empates = 0
@@ -9,37 +13,45 @@ let empates = 0
 function escolha(jogador) {
     const computer = escolhaComputer()
 
+    result.innerHTML = 'JO'
+    setTimeout(() => {result.innerHTML = 'KÊN'}, 1000)
+    setTimeout(() => {result.innerHTML = 'PO'}, 2000)
+
+    setTimeout (() => {
     if (jogador === computer) {
         empates ++
-        document.querySelector('.result').innerHTML = 'Empate!'
+        result.innerHTML = 'Empate!'
     } else if ((jogador === 'pedra' && computer === 'tesoura') ||
                (jogador === 'papel' && computer === 'pedra') ||
                (jogador === 'tesoura' && computer === 'papel')) {
         vitorias ++
-        document.querySelector('.result').innerHTML = 'Você Venceu!'
+        result.innerHTML = 'Você Venceu!'
     } else {
         derrotas ++
-        document.querySelector('.result').innerHTML = 'Você Perdeu!'
+        result.innerHTML = 'Você Perdeu!'
     }
 
-    document.querySelector('.score').innerHTML = `Vitorias: ${vitorias}, Derrotas: ${derrotas}, Empates: ${empates}`
+    score.innerHTML = `Vitorias: ${vitorias}, Derrotas: ${derrotas}, Empates: ${empates}`
+    // Informa a pontuação atual do jogador.
 
-    document.querySelector('.choices').innerHTML = `Você <img class="move-icon" src="imagens/${jogador}-emoji.png"> <img class="move-icon" src="imagens/${computer}-emoji.png"> Computador`
+    choices.innerHTML = `Você <img class="move-icon" src="imagens/${jogador}-emoji.png"> <img class="move-icon" src="imagens/${computer}-emoji.png"> Computador`
+    // Imprime na tela a jogada tanto do jogador quanto do computador.
+    }, 3000)
+    // Utilização da função setTimeout para imprimir na tela JO-KÊN-PO cronometrado e apos isso imprimpir o resultado.
 }
 
-
 function escolhaComputer() {
-    const randomNumber = Math.random();
-    return randomNumber < 1 / 3 ? 'pedra' :
-           randomNumber < 2 / 3 ? 'papel' :
-           'tesoura';
+    const randomNumber = Math.random()
+    return randomNumber < 1 / 3 ? 'pedra' : randomNumber < 2 / 3 ? 'papel' : 'tesoura'
+    // Retorna 'pedra' se o número aleatório for menor que 1/3, se não for passa para a próxima verificação e assim por diante.
 }
 
 function resetScore() {
     vitorias = 0
     derrotas = 0
     empates = 0
-    document.querySelector('.score').innerHTML = 'Vitorias: 0, Derrotas: 0, Empates: 0'
-    document.querySelector('.choices').innerHTML = ''
-    document.querySelector('.result').innerHTML = ''
+    score.innerHTML = 'Vitorias: 0, Derrotas: 0, Empates: 0'
+    choices.innerHTML = ''
+    result.innerHTML = ''
+    // Reseta toda as contagens para 0 e atualiza o HTML.
 }
