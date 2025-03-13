e74703d1adc06fb57c21966b7402766f3987faf6
WeskleyReis <weskleyff88004216@gmail.com>
2025-03-13 04:49:59 -0300

    Jokenpo update

--- JOKÊNPO/script.js
+++ JOKÊNPO/script.js
@@ -1,6 +1,6 @@
-document.getElementById('pedra').addEventListener('click', () => escolha('pedra'), false)
-document.getElementById('papel').addEventListener('click', () => escolha('papel'), false)
-document.getElementById('tesoura').addEventListener('click', () => escolha('tesoura'), false)
+document.getElementById('pedra').addEventListener('click', () => escolha('pedra'))
+document.getElementById('papel').addEventListener('click', () => escolha('papel'))
+document.getElementById('tesoura').addEventListener('click', () => escolha('tesoura'))
 
 let vitorias = 0
 let derrotas = 0
@@ -24,20 +24,15 @@ function escolha(jogador) {
 
     document.querySelector('.score').innerHTML = `Vitorias: ${vitorias}, Derrotas: ${derrotas}, Empates: ${empates}`
 
-    document.querySelector('.escolhas').innerHTML = `Você <img class="move-icon" src="imagens/${jogador}-emoji.png"> <img class="move-icon" src="imagens/${computer}-emoji.png"> Computador`
+    document.querySelector('.choices').innerHTML = `Você <img class="move-icon" src="imagens/${jogador}-emoji.png"> <img class="move-icon" src="imagens/${computer}-emoji.png"> Computador`
 }
 
+
 function escolhaComputer() {
-    const randomNumber = Math.random()
-    let computer
-    if (randomNumber < (1/3)) {
-        computer = 'pedra'
-    } else if (randomNumber < (2/3)) {
-        computer = 'papel'
-    } else {
-        computer = 'tesoura'
-    }
-    return computer
+    const randomNumber = Math.random();
+    return randomNumber < 1 / 3 ? 'pedra' :
+           randomNumber < 2 / 3 ? 'papel' :
+           'tesoura';
 }
 
 function resetScore() {
@@ -45,6 +40,6 @@ function resetScore() {
     derrotas = 0
     empates = 0
     document.querySelector('.score').innerHTML = 'Vitorias: 0, Derrotas: 0, Empates: 0'
-    document.querySelector('.escolhas').innerHTML = ''
+    document.querySelector('.choices').innerHTML = ''
     document.querySelector('.result').innerHTML = ''
 }
