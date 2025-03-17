6e52fc9d17d95d40d4105de3fa82a3bd7d498ed6
WeskleyReis <weskleyff88004216@gmail.com>
2025-03-16 22:56:47 -0300

    Update

--- Lista-de-Vendas/script.js
+++ Lista-de-Vendas/script.js
@@ -2,6 +2,8 @@ let totalDetergente = 0
 let totalCloro = 0
 let totalSabao = 0
 let totalDesinfetante = 0
+let totalLimpaAluminio = 0
+let totalAmaciante = 0
 let valoresAdicionados = [] // Lista para armazenar os valores adicionados
 
 function adicionarLista(classe, valor, container) {
@@ -30,23 +32,29 @@ function cadastrar() {
     let cloro = Number(document.querySelector('.cloro').value) || 0
     let sabao = Number(document.querySelector('.sabao').value) || 0
     let desinfetante = Number(document.querySelector('.desinfetante').value) || 0
+    let limpaAluminio = Number(document.querySelector('.limpa-aluminio').value) || 0
+    let amaciante = Number(document.querySelector('.amaciante').value) || 0
 
     adicionarLista('tabela-nome', nome, '.js-tabela-nomes')
     adicionarLista('tabela-detergente', detergente, '.js-tabela-detergente')
     adicionarLista('tabela-cloro', cloro, '.js-tabela-cloro')
     adicionarLista('tabela-sabao', sabao, '.js-tabela-sabao')
     adicionarLista('tabela-desinfetante', desinfetante, '.js-tabela-desinfetante')
+    adicionarLista('tabela-limpa-aluminio', limpaAluminio, '.js-tabela-limpa-aluminio')
+    adicionarLista('tabela-amaciante', amaciante, '.js-tabela-amaciante')
 
-    valoresAdicionados.push({ detergente, cloro, sabao, desinfetante})  // Armazena os valores para remoção correta
-    calculoTotal(detergente, cloro, sabao, desinfetante)
+    valoresAdicionados.push({ detergente, cloro, sabao, desinfetante, limpaAluminio, amaciante})  // Armazena os valores para remoção correta
+    calculoTotal(detergente, cloro, sabao, desinfetante, limpaAluminio, amaciante)
 }
 
 
-function calculoTotal(detergente, cloro, sabao, desinfetante) {
+function calculoTotal(detergente, cloro, sabao, desinfetante, limpaAluminio, amaciante) {
     totalDetergente += detergente
     totalCloro += cloro
     totalSabao += sabao
     totalDesinfetante += desinfetante
+    totalLimpaAluminio += limpaAluminio
+    totalAmaciante += amaciante
 
     // Armazena o total de cada item da lista
 
@@ -58,6 +66,8 @@ function atualizarTotais() {
     document.querySelector('.cloro-total').innerHTML = totalCloro
     document.querySelector('.sabao-total').innerHTML = totalSabao
     document.querySelector('.desinfetante-total').innerHTML = totalDesinfetante
+    document.querySelector('.limpa-aluminio-total').innerHTML = totalLimpaAluminio
+    document.querySelector('.amaciante-total').innerHTML = totalAmaciante
 
     // Atualiza na tela o total de cada item da lista
 }
@@ -76,14 +86,18 @@ function removerUltimaEntrada() {
     totalCloro -= ultimo.cloro
     totalSabao -= ultimo.sabao
     totalDesinfetante -= ultimo.desinfetante
+    totalLimpaAluminio -= ultimo.limpaAluminio
+    totalAmaciante -= ultimo.amaciante
 
     const classes = [
         ".js-tabela-nomes",
         ".js-tabela-detergente",
         ".js-tabela-cloro",
         ".js-tabela-sabao",
-        ".js-tabela-desinfetante"
-    ];
+        ".js-tabela-desinfetante",
+        ".js-tabela-limpa-aluminio",
+        ".js-tabela-amaciante"
+    ]
 
     classes.forEach(container => {
         const elementoContainer = document.querySelector(container)
